784af4e030f8f9d70ba82e0b467afe15f07c452e
DanieleF36 <daniele.femia@outlook.com>
2025-03-27 22:49:25 +0100

    added star field background

--- curriculum/src/App.tsx
+++ curriculum/src/App.tsx
@@ -1,17 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from './components/header/Header';
-import Hero from './components/Hero';
+
 import { Container } from 'react-bootstrap';
-import Contact from './components/Contact';
+import Header from './components/header/Header';
+import StarField from './components/intro/StarField';
+import MeteorShower from './components/intro/MeteorShower';
 
 function App() {
   return (
-    <Container fluid>
-      <Header />
-      <Hero />
-      <Contact/>
-    </Container>
+    <>
+      <StarField />
+      <MeteorShower />
+      <Container fluid className='content-wrapper'>
+        <Header />
+      </Container>
+    </>
   );
 }
 
